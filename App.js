52416dc6b31ec36ea2460f29ebde2a4583db699b
Rohit Dumka <torohitdumka@gmail.com>
2024-05-02 14:05:18 +0530

    parcel advs

--- App.js
+++ App.js
@@ -1,3 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//Parcel OP:-
+// Created a Server
+// HMR: Hot Module Replacement
+// File Watcher Algorithm
+// Bundling
+// Minify
+// Cleaning our Code 
+// Dev and Production Build 
+// Super fast build Algorithm
+// image optimization
+// Caching while development
+// Compression 
+// Compatible with other version of browser
+// HTTPS on devicePixelRatio
+// Port Number
+// Consistent Hashing Algorithm
+// Zero Config
+
 const heading  = React.createElement(
     "h1",
     {id: 'title1'},
